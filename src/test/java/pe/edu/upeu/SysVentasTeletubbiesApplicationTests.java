c28d88a99dd2d7ca1049e0efd316cc6a17e16fc2
Aldair-cloud <leonelsanchez@upeu.edu.pe>
2020-09-27 19:52:54 -0500

    al

--- src/test/java/pe/edu/upeu/SysVentasTeletubbiesApplicationTests.java
+++ src/test/java/pe/edu/upeu/SysVentasTeletubbiesApplicationTests.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import pe.edu.upeu.sysVentas.dao.PersonaDao;
+import pe.edu.upeu.sysVentas.dao.ProductoDao;
 import pe.edu.upeu.sysVentas.dao.RolDao;
 import pe.edu.upeu.sysVentas.entity.Persona;
+import pe.edu.upeu.sysVentas.entity.Producto;
 import pe.edu.upeu.sysVentas.entity.Rol;
 
 @SpringBootTest
@@ -22,7 +24,7 @@ class SysVentasTeletubbiesApplicationTests {
 		//System.out.println(rolDao.create(r));
 		System.out.println(rolDao.readAll());
 	}*/
-private PersonaDao personaDao;
+/*private PersonaDao personaDao;
 	@Test
 	void contextLoads() {
 		Persona p = new Persona();
@@ -30,5 +32,15 @@ private PersonaDao personaDao;
 		p.setApellidos("Mecha");
 		p.setDni(12121212);
 		System.out.println(personaDao.readAll());
+	}*/
+private ProductoDao productoDao;
+	@Test
+	void contextLoads() {
+		Producto p = new Producto();
+		p.setIdproducto(2);
+		p.setNomprod("jabon bolivar");
+		p.setPrecio(4);
+		p.setCantidad(1);
+		System.out.println(productoDao.readAll());
 	}
 }
