e2393a940b51b701701e4a51ddc2ff01a970a710
Aldair-cloud <leonelsanchez@upeu.edu.pe>
2020-09-27 19:17:57 -0500

    persona

--- src/test/java/pe/edu/upeu/SysVentasTeletubbiesApplicationTests.java
+++ src/test/java/pe/edu/upeu/SysVentasTeletubbiesApplicationTests.java
@@ -4,13 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import pe.edu.upeu.sysVentas.dao.PersonaDao;
 import pe.edu.upeu.sysVentas.dao.RolDao;
+import pe.edu.upeu.sysVentas.entity.Persona;
 import pe.edu.upeu.sysVentas.entity.Rol;
 
 @SpringBootTest
 class SysVentasTeletubbiesApplicationTests {
 @Autowired
-private RolDao rolDao;
+/*private RolDao rolDao;
 	@Test
 	void contextLoads() {
 		Rol r = new Rol();
@@ -19,6 +21,14 @@ private RolDao rolDao;
 		r.setNomrol("Teddy S");
 		//System.out.println(rolDao.create(r));
 		System.out.println(rolDao.readAll());
+	}*/
+private PersonaDao personaDao;
+	@Test
+	void contextLoads() {
+		Persona p = new Persona();
+		p.setNombre("Sett");
+		p.setApellidos("Mecha");
+		p.setDni(12121212);
+		System.out.println(personaDao.readAll());
 	}
-
 }
